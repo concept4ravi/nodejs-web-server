2049d7abbdc4f7a2bbd0c768f9564c9489380dd6
concept4ravi <concept4ravi@gmail.com>
2018-06-02 12:04:33 +0530

    Setup start script and heroku port change

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const port = process.env.PORT || 5000;
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine','hbs');
 
-app.use(function(req, res, next) {
-    res.render('maintanance.hbs',{
-        pageTitle: 'At work'
-    })
+// app.use(function(req, res, next) {
+//     res.render('maintanance.hbs',{
+//         pageTitle: 'At work'
+//     })
     
-  });
+//   });
 app.use(express.static(__dirname+'/public'));
 
 app.get('/',(req,res)=>{
@@ -28,6 +28,6 @@ app.get('/about',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log('Server started on port 3000');
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
 });
